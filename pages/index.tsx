c023f2b76e7f6dda7d6d575ad1a05e18fbc63daa
Jinkyou Son <nallwhy@gmail.com>
2022-11-04 18:15:39 +0900

    Apply SSG with file system

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from "../lib/posts"
 
-export default function Home() {
+type Post = { date: string, title: string, id: string }
+
+export async function getStaticProps() {
+  const allPosts = getSortedPostsData()
+
+  return {
+    props: {
+      allPosts
+    }
+  }
+}
+
+export default function Home({ allPosts }: { allPosts: Post[] }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +26,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section>
+        <h2>Blog</h2>
+        <ul>
+          {allPosts.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
